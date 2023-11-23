44674fe9b6f1402137a18b028906367baa897fce
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2023-11-23 10:02:57 -0300

    login_required

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
     
 class CarsListView(ListView):
     model = Car
@@ -15,6 +17,8 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
     
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -25,6 +29,7 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -33,6 +38,7 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
