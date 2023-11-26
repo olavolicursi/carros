1aad4178a0b67574ec03fb2565b0b0dc3aeb9305
olavolicursi <licursiolavo@gmail.com>
2023-11-26 13:31:47 -0300

    correção

--- cars/signals.py
+++ cars/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from cars.models import Car, CarInvetory
 from django.db.models import Sum
-from openai_api.client import get_car_ai_bio
+# from openai_api.client import get_car_ai_bio
 
 def car_inventory_update():
     cars_count = Car.objects.all().count()
@@ -14,13 +14,13 @@ def car_inventory_update():
         cars_value=cars_value
     )
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    if not instance.bio:
-        ai_bio = get_car_ai_bio(
-            instance.model, instance.brand, instance.model_year
-        )
-        instance.bio = ai_bio
+# @receiver(pre_save, sender=Car)
+# def car_pre_save(sender, instance, **kwargs):
+#     if not instance.bio:
+#         ai_bio = get_car_ai_bio(
+#             instance.model, instance.brand, instance.model_year
+#         )
+#         instance.bio = ai_bio
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
